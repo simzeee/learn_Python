f41d20c3683d69cde2dd9c6d5812977c4ab140c0
simzeee <jwsims816@gmail.com>
2024-06-11 19:15:09 -0400

    finish collatz, learn ternary

---
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,35 @@
+def steps(number):
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    steps = 0
+
+    while number != 1:
+        if number % 2 == 0:
+            number = number / 2
+            steps += 1
+        elif number % 2 != 0:
+            number = 3 * number + 1
+            steps += 1
+
+    return steps
+
+# solution 2 using ternary
+
+# def steps(number):
+#     if number <= 0:
+#         raise ValueError("Only positive integers are allowed")
+#     counter = 0
+#     while number != 1:
+#         number = number / 2 if number % 2 == 0 else number * 3 + 1
+#         counter += 1
+#     return counter
+
+# solution 3 using recursion
+
+# def steps(number):
+#     if number <= 0:
+#         raise ValueError("Only positive integers are allowed")
+#     if number == 1:
+#         return 0
+#     number = number / 2 if number % 2 == 0 else number * 3 + 1
+#     return 1 + steps(number)
