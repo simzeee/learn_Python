bea11f8c2e4ee2d303bad59b516790898ef889ce
simzeee <jwsims816@gmail.com>
2025-11-24 22:50:50 -0500

    finish rock paper scissors

--- 100_days_of_Python/4_day.py
+++ 100_days_of_Python/4_day.py
@@ -1,6 +1,59 @@
 import random
-friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
-print(random.choice(friends))
 
-random_index = random.randint(0, 4)
-print(friends[random_index])
+# friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+# print(random.choice(friends))
+
+# random_index = random.randint(0, 4)
+# print(friends[random_index])
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+options = [rock, paper, scissors]
+
+computer_choice = random.randint(0, 2)
+user_choice = int(
+    input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+)
+
+if user_choice >= 0 and user_choice <= 2:
+    print(options[user_choice])
+
+print("Computer chose:")
+print(options[computer_choice])
+
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number. You lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose!")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif user_choice > computer_choice:
+    print("You win!")
+elif user_choice == computer_choice:
+    print("Draw")
